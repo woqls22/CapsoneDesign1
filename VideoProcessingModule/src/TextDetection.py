15a5be417c40454a2195e35ff9991d6f13718912
LEEJAEBEEN <woqls226@gmail.com>
2020-04-30 03:01:09 +0900

    modify 0430

--- VideoProcessingModule/src/TextDetection.py
+++ VideoProcessingModule/src/TextDetection.py
@@ -1,90 +1,78 @@
-# import the necessary packages
-import numpy as np
 import cv2
+import numpy as np
+import matplotlib as plt
 import time
-
-# initialize the HOG descriptor/person detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-cv2.startWindowThread()
-fname = "./croppedimg/human/"
-# open webcam video stream
-cap = cv2.VideoCapture(
-    0)
-i = 0
-def draw_left_path(img,x,y,w,h):
-    start_point = x+w
-    cv2.line(img, (160-2*w,240), (start_point, y+int(h)), (255,0,0), 8) #4픽셀 선 그리기
-    cv2.line(img, (start_point, y+int(h)), (start_point+int(w/5),y+int(h-20)), (255, 0, 0), 10)
-    start_point = x+2*w
-    cv2.line(img, (250,240), (start_point, y+int(h)), (255,0,0), 8) #4픽셀 선 그리기
-   # cv2.line(img, (start_point, y+int(h)), (start_point+int(w/5),y+int(h-20)), (255, 0, 0), 10)
-    return img
-def draw_right_path(img, x, y, w, h):
-    start_point = x
-    cv2.line(img, (160+2*w, 240), (start_point, y+int(h)), (255, 0, 0), 8)  # 8픽셀 선 그리기
-    cv2.line(img, (start_point, y+int(h)), (start_point - int(w/5), y+int(h-20)), (255, 0, 0), 8)
-    start_point = abs(x-w)
-    cv2.line(img, (20, 240), (start_point, y + int(h)), (255, 0, 0), 8)  # 8픽셀 선 그리기
-   # cv2.line(img, (start_point, y + int(h)), (start_point - int(w / 5), y + int(h-20)), (255, 0, 0), 8)
-    return img
-while (True):
-    # Capture frame-by-frame
+fname = "./croppedimg/"
+index = 0
+prevTime = 0
+def bboxes(inp,prevTime):
+    #Frame을 인자로 전달받음
+    img = inp
     start = time.time()
-    ret, frame = cap.read()
-    # resizing for faster detection[240,160] [320 * 240]
-    frame = cv2.resize(frame, (320, 240))
-    # using a greyscale picture, also for faster detection
+    curTime = time.time()
+    # img2gray = cv2.imread(fname,0)
+    # img = cv2.namedWindow(img,cv2.WINDOW_NORMAL)
+    # img = cv2.resizeWindow(img,600,600)
+    img_final = inp
+    # img_final = cv2.namedWindow(fname,cv2.WINDOW_NORMAL)
+    # img_final = cv2.resizeWindow(fname,600,600)
+    img2gray = cv2.cvtColor(inp, cv2.COLOR_BGR2GRAY) #GRAY Image 8bit per pixel
+    ret, mask = cv2.threshold(img2gray, 180, 255, cv2.THRESH_BINARY) #threshold : distinguish background, object
+    image_final = cv2.bitwise_and(img2gray, img2gray, mask=mask) #bitwise
+    ret, new_img = cv2.threshold(img_final, 180, 255, cv2.THRESH_BINARY)  # Nfor black text , cv.THRESH_BINARY_IV
+    newimg = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY) #Gray Image converting
+    #newimg = cv2.GaussianBlur(newimg, (3,3),0)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # remove noise from image
+    #kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5,1))
+    # to manipulate the orientation of dilution , large x means horizonatally dilating  more, large y means vertically dilating more
+    #dilated = cv2.dilate(newimg, kernel, iterations=1)  # dilate
+   # erode = cv2.erode(newimg, kernel)
+    _,contours, _ = cv2.findContours(newimg, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)  # get contours
+    #cv2.CHAIN_APPROX_NONE: 모든 컨투어 포인트를 반환
+    for contour in contours:
+        # get rectangle bounding contour
+        [x, y, w, h] = cv2.boundingRect(contour)
+        # remove small false positives that aren't textq
+        # text인식하기. width, height
+        if w > 50 or h > 35 or w<25:
+            continue
+        if h / w > 1.0 or w / h > 2.0:
+            continue
+        if h>40 or w>70:
+            continue
+        if y>150:
+            continue
+        # draw rectangle around contour on original image
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        cv2.putText(img,"cropped", (x-50,y-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
+        #print("w : {0}, h : {1}".format(w, h))
+        # 이미지 주변 시작좌표를 기준으로 사각형틀그리기
+        cropped = img_final[y :y +  h , x : x + w]
+        #print("[Processing time ]:", time.time() - start)
+      #  M2 = cv2.getRotationMatrix2D((w / 2, h / 2), 270, 1)q
+      #  cropped = cv2.warpAffine(cropped,M2,(w,h))
+      #  cropped = cv2.resize(cropped, (32,32))
+        #cv2.imwrite(s, cropped)
+    # write original image with added contours to disk
+    img = cv2.resize(img, (320, 240))
+    sec = curTime - prevTime
+    prevTime = curTime
+    fps = 1/(sec)
+    #print ("Time {0} ".format(sec))
+    #print ("Estimated fps {0} ".format(fps))
+    str1 = ("FPS : {0}".format(int(fps)))
+    cv2.putText(img, str1, (0, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 255, 0),1)
+    cv2.imshow('captcha_result', img)
+    return prevTime
 
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
-    detectCount = 0
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-    for (xA, yA, xB, yB) in boxes:
-        # display the detected boxes in the colour picture
-        cv2.rectangle(frame, (xA, yA), (xB, yB),
-                      (0, 255, 0), 2)
-        cv2.putText(frame, "Detect", (xA - 50, yA - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
-        detectCount = detectCount+1
-        if(detectCount>1):
-            print("Waiting...")
-        else :
-            if(i%10 == 0):
-                cropped = frame[yA:yB,xA:xB]
-                #print("xA : {0}, xB : {1}, yA : {2}, yB : {3}".format(xA, xB,yA,yB))  # Print Width, Height of Cropped Area
-                i=0
-            if(xB < 190 and xA<130):
-                print("Left Side Detect.")
-                try:
-                    frame = draw_left_path(frame, xA,yA,xB-xA,yB-yA)
-                except:
-                    pass
-            elif(xA>130 and xB>190):
-                print("Right Side Detect")
-                try:
-                    frame = draw_right_path(frame, xA, yA, xB - xA, yB - yA)
-                except:
-                    pass
-            else:
-                try:
-                    frame = draw_right_path(frame, xA, yA, xB - xA, yB - yA)
-                except:
-                    pass
-                print("Center Side Detect")
-       # s = fname + str(i)+'.jpg'
-       # cv2.imwrite(s, cropped) # IMG File Write
-        print("time :", time.time() - start)
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+cap = cv2.VideoCapture(0) #동영상 파일 읽어옴
+while (cap.isOpened()):
+    ret, inp = cap.read() #프레임을 읽어옴, 읽어온 프레임을 인자로 bboxes 전달
+    if(ret): #success boolean
+        prevTime = bboxes(inp, prevTime)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Terminate Process..")
         break
+cap.release() #파일 닫아줌
 
-# When everything done, release the capture
-cap.release()
-# and release the output
-# finally, close the window
-cv2.destroyAllWindows()
-cv2.waitKey(1)
